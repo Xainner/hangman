f3765b3f97ac6df58d3f7a3fecb75b7f7cc31c66
Jose <jose.rojas38@utlatina.net>
2016-09-24 14:52:03 -0600

    Inicio del proyecto

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -8,8 +8,27 @@ namespace Hangman
 {
     class Program
     {
+
+        private static int lives;
+
+        private static string getUserName ()
+        {
+            string userName = "";
+            Console.WriteLine("Ingrese su nombre de jugador: ");
+            return userName = Console.ReadLine();
+        }
+
+        private static void initGameComponents ()
+        {
+            lives = 5;
+
+        }
+
         static void Main(string[] args)
         {
+            initGameComponents();
+            string userName = getUserName();
+
         }
     }
 }
